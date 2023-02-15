0d287ed9591ee36d5e82d2f80ee9acfe6faa4ad6
Michael <mconner3@ggc.edu>
2023-02-15 13:24:33 -0500

    Added Linear Gradient to background

--- StonksMobile/App.js
+++ StonksMobile/App.js
@@ -1,12 +1,33 @@
+import { useEffect } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Poppins_300Light_Italic, Poppins_400Regular } from '@expo-google-fonts/poppins';
+
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+  });
+
+  useEffect(() => {
+
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      
+      <LinearGradient
+        colors={['#B401FC', '#E91EAC']}
+        locations={[0.65, 0.95]}
+        style={{flex: 1, width: "100%", justifyContent: 'center', alignItems: 'center'}}
+        >
+        <Text style={styles.text}>Stonks</Text>
+        {/* <StatusBar style="auto" /> */}
+      </LinearGradient>
     </View>
   );
 }
@@ -18,4 +39,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'Poppins_400Regular',
+    fontSize: '48px',
+    color: '#ffffff',
+  }
 });
