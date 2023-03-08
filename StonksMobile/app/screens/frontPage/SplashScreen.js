d8a5cab743081487f84afced83d6687ee1ce1bdd
amunye1 <80010527+amunye1@users.noreply.github.com>
2023-03-08 16:52:14 -0500

    Added text to splash screen, also added submit button and text input.

--- StonksMobile/app/screens/frontPage/SplashScreen.js
+++ StonksMobile/app/screens/frontPage/SplashScreen.js
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { View, Text } from "react-native";
+import { View, Text,TextInput, Button } from "react-native";
 
 import SplashStyles from "./Styles"
 import AppStyles from "../../globals/styles/AppStyles";
@@ -14,6 +14,24 @@ const SplashScreen = () => {
                 style={SplashStyles.container}
             >
                 <Text style={SplashStyles.text}>STONKS</Text>
+                <TextInput
+        style={{height: 40}}
+        placeholder="Username"
+      />
+
+        <TextInput
+        style={{height: 40}}
+        placeholder="Password"
+      />
+
+      <Text>Forgot username or password</Text>
+      <Button
+  title="Submit"
+  color="#54EF46"
+  backgroundColor= ""
+  accessibilityLabel="Forgot Username or Password"
+/>
+
             </LinearGradient>
         </View>
     );
