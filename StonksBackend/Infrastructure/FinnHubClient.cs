6c492cd0d67ca8b45a9e6e3d9463d9770a8f9f6e
Michael <mconnerbusiness@gmail.com>
2023-03-21 22:13:26 -0400

    Updated URL var to use interpolation

--- StonksBackend/Infrastructure/FinnHubClient.cs
+++ StonksBackend/Infrastructure/FinnHubClient.cs
@@ -5,14 +5,14 @@ namespace StonksBackend.Infrastructure{
         private readonly HttpClient _httpClient;
         private const String _baseURL = "https://finnhub.io/api/v1";
         private const String _tokenParam = "&token=cfjc9epr01que34nu720cfjc9epr01que34nu72g";
-        private const String _stockQuote = "/quote?symbol=";
+        private const String _stockQuote = "/stock/candle?symbol=AAPL";
 
         public FinnHubClient(){
             _httpClient = new HttpClient();
         }
 
         public async Task<String> getStockInformation(String stock){
-            String fullPath = _baseURL + _stockQuote + stock + _tokenParam;
+            String fullPath = $"{_baseURL}{_stockQuote}&resolution=60&from={getUnixTime30DaysAgo()}&to={getUnixTimeNow()}{_tokenParam}";
 
             HttpResponseMessage response = await _httpClient.GetAsync(fullPath);
 
@@ -21,6 +21,14 @@ namespace StonksBackend.Infrastructure{
             }
 
             return await response.Content.ReadAsStringAsync();  
+        }
+
+        private int getUnixTimeNow(){
+            return (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+
+        private int getUnixTime30DaysAgo(){
+            return (int)DateTime.UtcNow.AddDays(-30).Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
         }
 
 
