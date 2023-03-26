7ae9fd2650a6ffa0fd835a5a7aac04a01a222add
Michael <mconnerbusiness@gmail.com>
2023-03-26 15:37:06 -0400

    Added Startup to Program configuration sequence

--- StonksBackend/Startup.cs
+++ StonksBackend/Startup.cs
@@ -1,15 +1,50 @@
 using StonksBackend.Domain;
 using StonksBackend.Domain.Entities;
-using StonksBackend.Infrastructure;
+using StonksBackend.Domain.DataClients;
+using StonksBackend.Infrastructure.Clients;
+using StonksBackend.Infrastructure.Repos;
 
-namespace StonksBackend {
-    public class Startup {
+namespace StonksBackend
+{
+    public class Startup
+    {
 
-        public void ConfigureServices(IServiceCollection services){
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
 
             services.AddScoped<IDataRepo<User>, UserDataRepo>();
             services.AddScoped<IDataRepo<Order>, OrderDataRepo>();
 
+            services.AddScoped<IDatabaseClient, DatabaseClient>();
+            string? str = _config.GetConnectionString("MongoDB");
+            services.AddSingleton<IConfiguration>(_config);
         }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+
     }
 }
