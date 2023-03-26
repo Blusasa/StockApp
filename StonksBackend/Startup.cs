70412808593cf1d9c65324a9be3f64561368827b
Michael <mconnerbusiness@gmail.com>
2023-03-26 17:49:28 -0400

    Updated dependencies in Startup

--- StonksBackend/Startup.cs
+++ StonksBackend/Startup.cs
@@ -3,6 +3,7 @@ using StonksBackend.Domain.Entities;
 using StonksBackend.Domain.DataClients;
 using StonksBackend.Infrastructure.Clients;
 using StonksBackend.Infrastructure.Repos;
+using StonksBackend.Services;
 
 namespace StonksBackend
 {
@@ -25,9 +26,8 @@ namespace StonksBackend
 
             services.AddScoped<IDataRepo<User>, UserDataRepo>();
             services.AddScoped<IDataRepo<Order>, OrderDataRepo>();
-
             services.AddScoped<IDatabaseClient, DatabaseClient>();
-            string? str = _config.GetConnectionString("MongoDB");
+            
             services.AddSingleton<IConfiguration>(_config);
         }
 
