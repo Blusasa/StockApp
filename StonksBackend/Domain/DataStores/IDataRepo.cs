a695acbc7257a8cd3fc04cf4956ea67bc8844b4b
Michael <mconnerbusiness@gmail.com>
2023-03-26 17:51:07 -0400

    DataRepo tasks no longer return results

--- StonksBackend/Domain/DataStores/IDataRepo.cs
+++ StonksBackend/Domain/DataStores/IDataRepo.cs
@@ -4,9 +4,9 @@ namespace StonksBackend.Domain
     public interface IDataRepo<T>
     {
 
-        Task<T> GetByIDAsync(String id);
-        Task<T> AddEntityAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task GetByIDAsync(String id);
+        Task AddEntityAsync(T entity);
+        Task DeleteAsync(T entity);
 
     }
 }
