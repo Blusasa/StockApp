2ada7d9edf0eac44ca084bcc3125891afde197c4
Michael <mconnerbusiness@gmail.com>
2023-03-30 17:22:28 -0400

    Trying npm thing

---
+++ StonksMobile/app/components/stockbox/StockBox.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { View } from "react-native";
+import StockInfo from "./StockInfo";
+
+
+const StockBox = () => {
+    const [stockInfo, setStockInfo] = ({});
+
+    useEffect(() => {
+
+    })
+
+    return(<View></View>);
+
+}
