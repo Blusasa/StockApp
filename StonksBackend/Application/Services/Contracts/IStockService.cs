6a4fb35f1acbd72c717ade1cc2966564d59d919a
Michael <mconnerbusiness@gmail.com>
2023-03-31 01:26:34 -0400

    Added interfaces for stock related clients/services

---
+++ StonksBackend/Application/Services/Contracts/IStockService.cs
@@ -0,0 +1,8 @@
+﻿using StonksBackend.Domain.Entities.Stocks;
+
+namespace StonksBackend.Application.Services.Contracts;
+
+public interface IStockService
+{
+    public Task<Stock> GetStockCandleData(string symbol);
+}
---
+++ StonksBackend/Domain/Interfaces/Clients/IMarketClient.cs
@@ -0,0 +1,8 @@
+﻿using StonksBackend.Domain.Entities.Stocks;
+
+namespace StonksBackend.Domain.Interfaces.Clients;
+
+public interface IMarketClient
+{ 
+    public Task<string> GetStockCandleDataAsync(String stockSymbol);
+}
