428a01b03c6640bf29b04f34f541bc82095b14d4
Michael <mconner3@ggc.edu>
2023-04-05 13:30:10 -0400

    Changed Stock CandleData Dict to nullable

--- StonksServer/StonksAPI/src/Domain/Entities/Stocks/Stock.cs
+++ StonksServer/StonksAPI/src/Domain/Entities/Stocks/Stock.cs
@@ -5,6 +5,6 @@ public class Stock
     public string? CompanyName { get; set; }
     public string? Symbol { get; set; }
     public Quote? Quote { get; set; }
-    public IDictionary<string, CandleData> CandleDatas { get; set; }
-    public ICollection<News> News { get; set; }
+    public IDictionary<string, IEnumerable<CandleData>>? CandleDatas { get; set; }
+    public ICollection<News>? News { get; set; }
 }
