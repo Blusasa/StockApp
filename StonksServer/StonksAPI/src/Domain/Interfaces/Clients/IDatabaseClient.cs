ebeeedd717336a98f183c81006a55888f3ed323b
Michael <mconnerbusiness@gmail.com>
2023-04-06 23:28:57 -0400

    Database Interface now exposes tables/collections of specific domain entities

--- StonksServer/StonksAPI/src/Domain/Interfaces/Clients/IDatabaseClient.cs
+++ StonksServer/StonksAPI/src/Domain/Interfaces/Clients/IDatabaseClient.cs
@@ -1,11 +1,21 @@
 
+using MongoDB.Driver;
+using StonksAPI.Domain.Entities;
+
 namespace StonksAPI.Domain.Interfaces.Clients{
     public interface IDatabaseClient{
+            //
+            // public Task CreateRecord<T>(T entity) where T: class;
+            //
+            // public Task UpdateRecord<T>(T entity);
+            //
+            // // public void Set(string collectionName);
+            //
+            // public IMongoDatabase GetDatabase();
+            //
+            // public IMongoCollection<T> GetCollection<T>(IMongoDatabase db, string name);
 
-            public Task CreateRecord<T>(T entity) where T: class;
-
-            public Task UpdateRecord<T>(T entity);
-
-            public void Set(string collectionName);
+            public IMongoCollection<AppUser> Users { get; }
+            public IMongoCollection<Group> Groups { get;  }
     }
 }
