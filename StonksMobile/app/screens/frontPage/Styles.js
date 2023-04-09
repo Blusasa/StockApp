d873e160e94371ea90ff397cfeac70c604375d82
Michael <mconnerbusiness@gmail.com>
2023-04-09 00:13:24 -0400

    Custom reusable button first iteration

--- StonksMobile/app/screens/frontPage/Styles.js
+++ StonksMobile/app/screens/frontPage/Styles.js
@@ -3,11 +3,9 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     text: {
-        fontSize: '48px',
+        fontSize: 48,
         color: '#ffffff',
     },
     textInput: {
