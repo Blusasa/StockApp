7ff68d04ab8c0b991988fb3aab885bd50046aad3
Michael <mconnerbusiness@gmail.com>
2023-04-09 22:59:39 -0400

    Added styles to tab navigator

--- StonksMobile/app/navigation/TabNavigator.js
+++ StonksMobile/app/navigation/TabNavigator.js
@@ -1,20 +1,66 @@
-import * as React from 'react';
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import HomePage from '../screens/homepage/HomePage';
-import Groups from '../screens/frontPage/Groups';
-import Profile from '../screens/frontPage/Profile';
+import Groups from '../screens/userGroupsPage/UserGroupsScreen';
+import Profile from '../screens/profilePage/Profile';
+import HomeScreen from '../screens/homePage/HomeScreen';
+
+import Ionicons from '../assets/rneIcons/Ionicons';
+import UserAssets from '../screens/userAssetsPage/UserAssets';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs= ()=> {
-    return(
-        
-        <Tab.Navigator>
-            {/* <Tab.Screen name ="HomePage" component={HomePage}/> */}
-            <Tab.Screen name ="Groups" component={Groups}/>
-            <Tab.Screen name ="Profile" component={Profile}/>
+const Tabs = () => {
+
+    const tabScreenOptions = {
+        "headerShown": false,
+        "tabBarHideOnKeyboard": true,
+        "tabBarStyle": {
+            "backgroundColor": "#B401FC"
+        },
+        "tabBarLabelPosition": "below-icon"
+    }
+
+    return (
+
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarHideOnKeyboard: true,
+                tabBarLabelPosition: "below-icon",
+                tabBarActiveBackgroundColor: "white",
+                tabBarActiveTintColor: "black",
+                tabBarInactiveTintColor: "white",
+                tabBarInactiveBackgroundColor: "#B401FC",
+                tabBarIcon: ({focused}) => {
+                    let iconName;
+
+                    switch(route.name){
+                        case "Home":
+                            iconName = focused ? "home-outline" : "home";
+                            break;
+                        case "Assets":
+                            iconName = focused ? "bar-chart-outline" : "bar-chart";
+                            break;
+                        case "Groups":
+                            iconName = focused ? "people-outline" : "people";
+                            break;
+                        case "Profile":
+                            iconName = focused ? "md-settings-outline" : "md-settings";
+                            break;
+                    }
+
+                    return <Ionicons name={iconName}/>
+                },
+            })}
+
+        >
+            
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Assets" component={UserAssets}/>
+            <Tab.Screen name="Groups" component={Groups} />
+            <Tab.Screen name="Profile" component={Profile} />
         </Tab.Navigator>
-        
+
     );
 }
 
