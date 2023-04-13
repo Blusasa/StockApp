4c806147be2e31a7a2986e41a4914b0b0443c69d
Michael <mconnerbusiness@gmail.com>
2023-04-12 22:59:33 -0400

    Updated stockbox to match design specs

--- StonksMobile/app/components/stockbox/StockBox.js
+++ StonksMobile/app/components/stockbox/StockBox.js
@@ -2,21 +2,51 @@ import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import api from "../../api/Api";
 import endpoints from "../../api/Endpoints";
+import AppStyles from "../../globals/styles/AppStyles";
+import StockBoxStyles from "./StockBoxStyles";
+import FeatherIcon from "../../assets/rneIcons/FeatherIcon";
 
-const StockBox = () => {
+const StockBox = ({ quote }) => {
     const [stockInfo, setStockInfo] = useState({});
 
+
+
     useEffect(() => {
-        api.get(endpoints.getQoute("msft"))
-                        .then((res) => {
-                            console.log(res.data.quote.quote);
-                            setStockInfo(res.data.quote);
-                        }).catch((err) => {});
+        // api.get(endpoints.getQoute("msft"))
+        //     .then((res) => {
+        //         console.log(res.data.quote);
+        //         setStockInfo(res.data.quote);
+        //     }).catch((err) => { });
+
+        setStockInfo({
+            currentPrice: 282.83,
+            percentChange: -2.27883
+        })
     }, []);
 
-    if(!stockInfo || !stockInfo.currentPrice) return null;
+    if (!stockInfo || !stockInfo.currentPrice) return null;
 
-    return(<View>{stockInfo.currentPrice}</View>);
+    const stockIsNegative = (
+        stockInfo.percentChange < 0 ? true : false
+    );
+
+    return (
+        <View style={StockBoxStyles.shadow}>
+        <View style={StockBoxStyles.boxContainer}>
+            <View style={[StockBoxStyles.infoContainer]}>
+                <Text style={[AppStyles.text, {fontSize: 20}]}>Microsoft</Text>
+                <Text style={[AppStyles.text, { color: "grey", fontSize: 16, alignSelf: "flex-start"}]}>MSFT</Text>
+            </View>
+            <View style={StockBoxStyles.pricingContainer}>
+                <Text style={[AppStyles.text, {fontSize: 20}]}>${stockInfo.currentPrice}</Text>
+                <View style={StockBoxStyles.percentContainer}>
+                    {stockIsNegative ? <FeatherIcon name={"arrow-down-left"} color={"#ff0000"}/> : <FeatherIcon name={"arrow-up-right"} color={"#00ff00"}/>}
+                    <Text style={[AppStyles.text, {color: stockIsNegative ? "#ff0000" : "#00ff00", fontSize: 16, alignSelf: "flex-end"}]}>{stockInfo.percentChange.toFixed(2)}%</Text>
+                </View>
+            </View>
+        </View>
+        </View>
+    );
 
 }
 
