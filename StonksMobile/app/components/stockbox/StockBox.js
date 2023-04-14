710ea60c52b24dcf6e3425c5588ac379c163493e
Michael <mconnerbusiness@gmail.com>
2023-04-13 20:49:50 -0400

    StockBox now uses passed in data

--- StonksMobile/app/components/stockbox/StockBox.js
+++ StonksMobile/app/components/stockbox/StockBox.js
@@ -7,41 +7,22 @@ import StockBoxStyles from "./StockBoxStyles";
 import FeatherIcon from "../../assets/rneIcons/FeatherIcon";
 
 const StockBox = ({ quote }) => {
-    const [stockInfo, setStockInfo] = useState({});
-
-
-
-    useEffect(() => {
-        // api.get(endpoints.getQoute("msft"))
-        //     .then((res) => {
-        //         console.log(res.data.quote);
-        //         setStockInfo(res.data.quote);
-        //     }).catch((err) => { });
-
-        setStockInfo({
-            currentPrice: 282.83,
-            percentChange: -2.27883
-        })
-    }, []);
-
-    if (!stockInfo || !stockInfo.currentPrice) return null;
-
     const stockIsNegative = (
-        stockInfo.percentChange < 0 ? true : false
+        quote.percentChange < 0 ? true : false
     );
 
     return (
         <View style={StockBoxStyles.shadow}>
         <View style={StockBoxStyles.boxContainer}>
             <View style={[StockBoxStyles.infoContainer]}>
-                <Text style={[AppStyles.text, {fontSize: 20}]}>Microsoft</Text>
-                <Text style={[AppStyles.text, { color: "grey", fontSize: 16, alignSelf: "flex-start"}]}>MSFT</Text>
+                <Text style={[AppStyles.text, {fontSize: 20}]}>{quote.name}</Text>
+                <Text style={[AppStyles.text, { color: "grey", fontSize: 16, alignSelf: "flex-start"}]}>{quote.symbol}</Text>
             </View>
             <View style={StockBoxStyles.pricingContainer}>
-                <Text style={[AppStyles.text, {fontSize: 20}]}>${stockInfo.currentPrice}</Text>
+                <Text style={[AppStyles.text, {fontSize: 20}]}>${quote.currentPrice}</Text>
                 <View style={StockBoxStyles.percentContainer}>
                     {stockIsNegative ? <FeatherIcon name={"arrow-down-left"} color={"#ff0000"}/> : <FeatherIcon name={"arrow-up-right"} color={"#00ff00"}/>}
-                    <Text style={[AppStyles.text, {color: stockIsNegative ? "#ff0000" : "#00ff00", fontSize: 16, alignSelf: "flex-end"}]}>{stockInfo.percentChange.toFixed(2)}%</Text>
+                    <Text style={[AppStyles.text, {color: stockIsNegative ? "#ff0000" : "#00ff00", fontSize: 16, alignSelf: "flex-end"}]}>{quote.percentChange.toFixed(2)}%</Text>
                 </View>
             </View>
         </View>
