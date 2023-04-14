22d90b9d5c0517d9613b447441ac4b67077420d1
Michael <mconnerbusiness@gmail.com>
2023-04-13 20:49:10 -0400

    Updated style references

--- StonksMobile/app/screens/homepage/HomeScreen.js
+++ StonksMobile/app/screens/homepage/HomeScreen.js
@@ -2,13 +2,14 @@ import { View, Text,TextInput } from "react-native";
 import appStyles2 from "../../globals/styles/AppStyles2";
 import PricesComponent from "./PricesComponent";
 import CustomTextInput from "../../components/CustomTextInput";
+import StockBox from "../../components/stockbox/StockBox";
+import AppStyles from "../../globals/styles/AppStyles";
 
 const HomeScreen = (props) => {
     return (
-        <View style ={appStyles2.flexContainer}>
-            <Text style={appStyles2.text}>STONKS</Text>
+        <View style ={[AppStyles.flexContainer, AppStyles.mainApp]}>
+            <Text style={AppStyles.textSemiBold}>STONKS</Text>
             <CustomTextInput placeholderText={"Search a Stock"} styles={{width: 300}}/>
-        
             <PricesComponent/>
         </View>
     );
