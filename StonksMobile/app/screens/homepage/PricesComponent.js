2b4dac6e321857274cb564ca77f061e28b531930
Michael <mconnerbusiness@gmail.com>
2023-04-13 20:50:43 -0400

    Price component simulates api fetch and passing data as props

--- StonksMobile/app/screens/homepage/PricesComponent.js
+++ StonksMobile/app/screens/homepage/PricesComponent.js
@@ -1,27 +1,48 @@
-import { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { useState, useEffect, useReducer } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
 import appStyles2 from "../../globals/styles/AppStyles2";
 import api from "../../api/Api";
 import endpoints from "../../api/Endpoints";
+import StockBox from "../../components/stockbox/StockBox";
+import AppStyles from "../../globals/styles/AppStyles";
 
 const PricesComponent = () => {
-    const [quoteData, setQouteData] = useState(undefined);
+    const [quoteData, setQouteData] = useState(null);
+
+    const stocks = [
+        {
+          name: "Stock1",
+          symbol: "STK1",
+          currentPrice: 100,
+          percentChange: 0.05
+        },
+        {
+          name: "Stock2",
+          symbol: "STK2",
+          currentPrice: 50,
+          percentChange: -0.02
+        },
+        {
+          name: "Stock3",
+          symbol: "STK3",
+          currentPrice: 75,
+          percentChange: 0.1
+        }
+    ];
 
     useEffect(() => {
-        api.get(endpoints.getQoute('AAPL'))
-            .then((res) => {
-                console.log(res.data);
-                setQouteData(res.data.quote);
-            }).catch((err) => { });
-    }, []);
+        setQouteData(stocks);
+    }, [])
 
 
-    if (!quoteData) return null;
+    if(!quoteData) {
+        return <ActivityIndicator color={"#E91EAC"} size={"large"}/>;
+    }
 
     return (
-        <View style={appStyles2.flexContainer}>
+        <View style={[AppStyles.flexContainer, AppStyles.mainApp]}>
+            {quoteData.map((value, index) => <StockBox key={value + index} quote={value}/>)}
 
-            <Text style={appStyles2.text}>Apple Current $: {quoteData.currentPrice}</Text>
         </View>
     );
 };
