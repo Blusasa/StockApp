c5fdc0118903a4c3654ad5180f419cff2baa0e0d
Michael <mconnerbusiness@gmail.com>
2023-04-13 20:53:24 -0400

    Updated code spacing to match standards

--- StonksMobile/App.js
+++ StonksMobile/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useEffect } from 'react';
 
 import { View } from 'react-native';
-import { useFonts, Poppins_300Light_Italic, 
-  Poppins_400Regular, Poppins_700Bold, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import {
+  useFonts, Poppins_300Light_Italic,
+  Poppins_400Regular, Poppins_700Bold, Poppins_600SemiBold
+} from '@expo-google-fonts/poppins';
 
 import Navigation from './app/navigation/MainAppNavigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -16,15 +18,15 @@ export default function App() {
     Poppins_600SemiBold
   });
 
-  useEffect(() => {}, [fontsLoaded]);
+  useEffect(() => { }, [fontsLoaded]);
 
   if (!fontsLoaded) return null;
 
   return (
     <SafeAreaProvider>
-    <View style={{flex: 1}}>
-      <Navigation/>
-    </View>
+      <View style={{ flex: 1 }}>
+        <Navigation />
+      </View>
     </SafeAreaProvider>
   );
 }
