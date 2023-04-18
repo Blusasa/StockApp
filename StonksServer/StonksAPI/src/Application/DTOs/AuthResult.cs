23cee369183036b4800d579bda6926c9a994cb33
Michael <mconnerbusiness@gmail.com>
2023-04-18 19:40:10 -0400

    Pulled Auth routes to separate controller and added AuthResult model

---
+++ StonksServer/StonksAPI/src/Application/DTOs/AuthResult.cs
@@ -0,0 +1,9 @@
+﻿namespace StonksAPI.Helpers;
+
+public class AuthResult
+{
+    public string Token { get; set; }
+    
+    public bool Authenticated { get; set; }
+    public List<string> errors { get; set; }
+}
---
+++ StonksServer/StonksAPI/src/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StonksAPI.Application.DTOs;
+using StonksAPI.Application.Services.Contracts;
+using StonksAPI.Configs;
+
+namespace StonksAPI.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly JwtConfig _jwtConfig;
+    private readonly IUserService _userService;
+
+
+    public AuthController(IUserService userService, IOptions<JwtConfig> config)
+    {
+        _userService = userService;
+        _jwtConfig = config.Value;
+    }
+
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> Register([FromBody] RegistrationDTO registration)
+    {
+        var result = await _userService.CreateUser(registration);
+
+        if (result.Authenticated)
+        {
+            return Created("/login", result);
+        }
+
+        return BadRequest(result.errors);
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDTO login)
+    {
+        var result = await _userService.Login(login);
+
+        if (result.Authenticated) return Ok(result);
+        return Unauthorized(result);
+    }
+}
