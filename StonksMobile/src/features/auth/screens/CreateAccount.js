c510712b5aa2b416c8aff3c5ec27c23117d297fc
Michael <mconnerbusiness@gmail.com>
2023-08-20 19:54:55 -0400

    CreateAccount page converted to themes

--- StonksMobile/src/features/auth/screens/CreateAccount.js
+++ StonksMobile/src/features/auth/screens/CreateAccount.js
@@ -1,44 +1,42 @@
 import { LinearGradient } from "expo-linear-gradient";
-import {Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import { useState } from "react"
+
+import { useTheme, commonStyles } from "../../../theme";
 
 import CustomTextInput from "../../../components/CustomTextInput";
-import AppStyles from "../../../theme/AppStyles";
-
 import CustomBtn from "../../../components/CustomButton";
 
 const CreateAccount = ({navigation}) => {
+  const [containerSize, setContainerSize] = useState({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0
+  });
+
+  const theme = useTheme();
+  const componentStyles = styles(theme, containerSize.height);
 
   const submitInfo = () => {
       navigation.replace("MainAppPages");
   }
 
-  const inputFields = [
-    "Username",
-    "Email",
-    "Phone Number",
-    "Password",
-    "Confirm Password"
-  ];
-
   return (
       <LinearGradient
         colors={['#B401FC', '#E91EAC']}
         locations={[0.65, 0.95]}
-        style={AppStyles.flexContainer}
+        style={[componentStyles.mainContainer]}
       >
-        <Text style={[AppStyles.textSemiBold, pageStyles.text, {fontSize: 30}]}>Create Account</Text>
+        <Text style={[componentStyles.text]}>Create Account</Text>
 
-        {/* <FlatList
-          data={inputFields}
-          renderItem={({item}) => <CustomTextInput placeholderText={item} styles={{width: 350,}} hideGradient={true}/>}
-          style={{flexShrink: 1, borderColor: "gold", borderWidth: 3}}
-        ></FlatList> */}
-
-        <CustomTextInput placeholderText={"Username"} styles={{width: 300}} hideGradient={true}/>
-        <CustomTextInput placeholderText={"Email"} styles={{width: 300}} hideGradient={true}/>
-        <CustomTextInput placeholderText={"Phone Number"} styles={{width: 300}} hideGradient={true}/>
-        <CustomTextInput placeholderText={"Password"} styles={{width: 300}} hideGradient={true}/>
-        <CustomTextInput placeholderText={"Confirm Password"} styles={{width: 300}} hideGradient={true}/>
+        <View style={[componentStyles.fieldsContainer]}>
+          <CustomTextInput placeholderText={"Username"} styles={{width: 300}} hideGradient={true}/>
+          <CustomTextInput placeholderText={"Email"} styles={{width: 300}} hideGradient={true}/>
+          <CustomTextInput placeholderText={"Phone Number"} styles={{width: 300}} hideGradient={true}/>
+          <CustomTextInput placeholderText={"Password"} styles={{width: 300}} hideGradient={true}/>
+          <CustomTextInput placeholderText={"Confirm Password"} styles={{width: 300}} hideGradient={true}/>
+        </View>
 
         <CustomBtn text={"SUBMIT"} styles={{width: 150}} onPress={submitInfo}></CustomBtn>
 
@@ -46,12 +44,18 @@ const CreateAccount = ({navigation}) => {
   );
 };
 
-const pageStyles = StyleSheet.create({
+const styles = (theme) => StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    ...commonStyles.flexColFullCenter,
+  },
+  fieldsContainer: {
+    flex: 1,
+    ...commonStyles.flexColCenter,
+  },
   text: {
-      fontSize: 70,
-      textShadowColor: "#000000",
-      textShadowOffset: { width: 5, height: 5},
-      elevation: 5
+      ...theme.fonts.title,
+      fontSize: 30,
   },
 });
 
